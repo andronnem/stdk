c5bb7d712a610e3cdd26e1ba9c147c3acbf2e8a7
andronnem <andronnem@ukr.net>
2026-07-23 19:15:20 +0300

    Create descriptions.js

---
+++ data/descriptions.js
@@ -0,0 +1,35 @@
+import Sequence from "../models/sequence.js";
+
+export default [
+
+    new Sequence(
+        "Fibonacci",
+        "Each number is the sum of the previous two."
+    ),
+
+    new Sequence(
+        "Prime Numbers",
+        "Numbers divisible only by one and themselves."
+    ),
+
+    new Sequence(
+        "Look-and-Say",
+        "Each term describes the previous one."
+    ),
+
+    new Sequence(
+        "Collatz",
+        "Generated using the Collatz rule."
+    ),
+
+    new Sequence(
+        "Triangular Numbers",
+        "Sum of natural numbers."
+    ),
+
+    new Sequence(
+        "Perfect Squares",
+        "Squares of positive integers."
+    )
+
+];
